389398fc2ce86bf71a54b31e2f0589f5714fbf51
FFFFFRYGIO <42962305+FFFFFRYGIO@users.noreply.github.com>
2025-04-25 05:01:52 +0200

    do wavelet_transform only for one dimension of data, skip analysis for second metrix

--- wavelet_transform.py
+++ wavelet_transform.py
@@ -5,6 +5,10 @@ from numpy.typing import NDArray
 
 def wavelet_transform(signal: NDArray, wavelet) -> tuple[NDArray, NDArray]:
     """Run wavelet transform"""
+
+    if signal.ndim == 2:
+        signal = signal[:, 0]
+
     cA, cD = pywt.dwt(signal, wavelet)
 
     return cA, cD
