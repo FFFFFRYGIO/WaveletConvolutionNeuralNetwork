36ccc09497cc6e61143a7f3b64ec728e5673d688
FFFFFRYGIO <42962305+FFFFFRYGIO@users.noreply.github.com>
2025-04-25 03:28:10 +0200

    Add utils with get_signal_subset and get_qrs_peaks

---
+++ data_getter/utils.py
@@ -0,0 +1,33 @@
+"""Help functions for all data getters."""
+from numpy.core.multiarray import ndarray
+from numpy.typing import NDArray
+from scipy.signal import find_peaks
+
+
+def get_signal_subset(signal: ndarray, signal_frequency: int, seconds: int | None = None) -> ndarray:
+    """Get specified time for signal."""
+
+    if seconds:
+        duration_samples = int(seconds * signal_frequency)
+        signal = signal[:duration_samples]
+
+    return signal
+
+
+def get_qrs_peaks(signal: NDArray) -> NDArray:
+    """Get qrs peaks times"""
+
+    # Main setup
+    qrs_height_factor = 1.0
+    qrs_min_distance = 0.2
+
+    height_thresh = signal.mean() + qrs_height_factor * signal.std()
+    min_dist_samples = int(qrs_min_distance * signal.shape[0])
+
+    peaks, _ = find_peaks(
+        signal,
+        height=height_thresh,
+        distance=min_dist_samples
+    )
+
+    return peaks
