8959af4225cf140d0fc8834b1a1c7157924f702b
FFFFFRYGIO <42962305+FFFFFRYGIO@users.noreply.github.com>
2025-04-25 03:29:48 +0200

    remove import from the main script outline

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 """Main script to run expected analysis"""
 
-from data_getter.from_github.get_data import get_from_github
-
 
 def main():
     """Get signals, create wavelet transforms, plot results"""
