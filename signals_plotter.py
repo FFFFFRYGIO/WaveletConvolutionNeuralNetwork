0b82b2417d25916cff11ae021468229362602c1e
FFFFFRYGIO <42962305+FFFFFRYGIO@users.noreply.github.com>
2025-04-25 05:50:21 +0200

    fix plotting different number of levels of decomposition

--- signals_plotter.py
+++ signals_plotter.py
@@ -13,7 +13,6 @@ class SignalsPlotter:
 
     def __init__(self) -> None:
         self.signals_set: list[tuple[NDArray, str, NDArray, int, NDArray, NDArray, str]] = []
-        self.num_levels = 2
 
     def add_signal_with_analysis(
             self, signal_content: tuple[NDArray, str, NDArray, int, NDArray, NDArray, str]
@@ -21,18 +20,30 @@ class SignalsPlotter:
         """Add signal to signals_set with its tas, qrs_peaks and frequency and wavelet transform"""
         self.signals_set.append(signal_content)
 
+    def get_max_num_levels(self) -> int:
+        """Get maximum number of levels for wavelets decomposition"""
+        max_num_levels = 0
+        for signal, tag, qrs_peaks, freq, cA, cDs, wavelet in self.signals_set:
+            cDs_number = len(cDs)
+            if cDs_number > max_num_levels:
+                max_num_levels = cDs_number
+
+        return max_num_levels
+
     def plot_signals(self) -> None:
         """Plot all signals added to signals plotter"""
 
         squeeze_when_one_signal = False
 
+        num_levels = self.get_max_num_levels()
+
         fig, axs = plt.subplots(
-            4, len(self.signals_set),
-            figsize=(10 * len(self.signals_set), 3 * (self.num_levels + 2)),
+            num_levels + 3, len(self.signals_set),
+            figsize=(6 * len(self.signals_set), 2 * (num_levels + 3)),
             squeeze=squeeze_when_one_signal
         )
 
-        for i, (signal, tag, qrs_peaks, freq, cA, cD, wavelet) in enumerate(self.signals_set):
+        for i, (signal, tag, qrs_peaks, freq, cA, cDs, wavelet) in enumerate(self.signals_set):
             time = np.arange(len(signal)) / freq
 
             if signal.ndim == 1:
@@ -50,8 +61,8 @@ class SignalsPlotter:
             axs[0, i].set_ylabel("Amplitude [mV]")
             axs[0, i].grid(True)
 
-            for coef_number, coef in enumerate([cA, cD]):
-                level_name = "A" if coef_number == 0 else f"D{self.num_levels - coef_number}"
+            for coef_number, coef in enumerate([cA] + cDs):
+                level_name = "A" if coef_number == 0 else f"D{num_levels - coef_number}"
                 axs[coef_number + 1, i].plot(np.arange(len(coef)) / freq, coef)
                 axs[coef_number + 1, i].set_title(f"Level {level_name} ({wavelet})")
                 axs[coef_number + 1, i].set_xlabel("Time [s]")
@@ -60,11 +71,11 @@ class SignalsPlotter:
             wavelet_function = pywt.Wavelet(wavelet)
             phi, psi, x = wavelet_function.wavefun()
 
-            axs[self.num_levels + 1, i].plot(x, psi)
-            axs[self.num_levels + 1, i].set_title(f"Wavelet ψ(t) – {wavelet}")
-            axs[self.num_levels + 1, i].grid(True)
-            axs[self.num_levels + 1, i].set_xlabel("t")
-            axs[self.num_levels + 1, i].set_ylabel("ψ(t)")
+            axs[-1, i].plot(x, psi)
+            axs[-1, i].set_title(f"Wavelet ψ(t) – {wavelet}")
+            axs[-1, i].grid(True)
+            axs[-1, i].set_xlabel("t")
+            axs[-1, i].set_ylabel("ψ(t)")
 
         plt.tight_layout()
         plt.show()
