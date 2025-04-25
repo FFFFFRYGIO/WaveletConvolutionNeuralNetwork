a407a408a4e8cf398bf20f07bfe86563800e916c
FFFFFRYGIO <42962305+FFFFFRYGIO@users.noreply.github.com>
2025-04-25 03:29:11 +0200

    add main script outline

---
+++ main.py
@@ -0,0 +1,11 @@
+"""Main script to run expected analysis"""
+
+from data_getter.from_github.get_data import get_from_github
+
+
+def main():
+    """Get signals, create wavelet transforms, plot results"""
+
+
+if __name__ == '__main__':
+    main()
