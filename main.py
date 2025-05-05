47a0889c97db4af1d830c9ac701c381447ca39f7
FFFFFRYGIO <42962305+FFFFFRYGIO@users.noreply.github.com>
2025-05-05 20:16:16 +0200

    move getting signals data to separate function

--- main.py
+++ main.py
@@ -7,23 +7,24 @@ from plotters.signals_plotter import SignalsPlotter
 from wavelet_transform import discrete_wavelet_transform
 
 
+def get_signals_data(
+        signal_time: int,
+) -> list[tuple[NDArray, str, NDArray, dict[str, int]] | tuple[list[tuple[NDArray, str, NDArray], dict[str, int]]]]:
+    """Get specified number of signals with its details information."""
+    h_signals_data = get_from_mit_bih_arrhythmia_database(['100', '103'], signal_time)
+    a_signals_data = get_from_mit_bih_arrhythmia_database(['108', '113'], signal_time)
+    af_signals_data = get_from_af_termination_challenge(['n01', 'n02'], signal_time)
+    return h_signals_data + a_signals_data + af_signals_data
+
+
 def dwt_plotting(wavelet: str, signal_time: int, decomposition_levels: int = 2):
     """Get signal, get wavelet transforms, finally plot them."""
 
     signals_plotter = DWTDecompositionPlotter()
 
-    h_signals_data = get_from_mit_bih_arrhythmia_database(['100', '103', '108', '113'], signal_time)
+    signals_data = get_signals_data(signal_time)
 
-    for signal, tag, qrs_peaks, fields in h_signals_data:
-        print(signal.shape, tag, qrs_peaks.shape, fields)
-
-        cA, cDs = discrete_wavelet_transform(signal, wavelet, level=decomposition_levels)
-
-        signals_plotter.add_signal_with_analysis((signal, tag, qrs_peaks, fields['fs'], cA, cDs, wavelet))
-
-    af_signals_data = get_from_af_termination_challenge(['n01', 'n02'], signal_time)
-
-    for signal, tag, qrs_peaks, fields in af_signals_data:
+    for signal, tag, qrs_peaks, fields in signals_data:
         print(signal.shape, tag, qrs_peaks.shape, fields)
 
         cA, cDs = discrete_wavelet_transform(signal, wavelet, level=decomposition_levels)
