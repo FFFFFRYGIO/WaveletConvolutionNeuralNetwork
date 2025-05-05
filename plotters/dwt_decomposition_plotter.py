18f70b646dbdb7590a7ead61160672001816732d
FFFFFRYGIO <42962305+FFFFFRYGIO@users.noreply.github.com>
2025-05-05 20:18:37 +0200

    add abstract method add_signal_with_analysis, method for get_max_num_decomposition_levels, static method plot_ecg_signal, static method plot_wavelet for SignalsPlotter, adjust DWTDecompositionPlotter for this change

--- plotters/dwt_decomposition_plotter.py
+++ plotters/dwt_decomposition_plotter.py
@@ -16,31 +16,21 @@ class DWTDecompositionPlotter(SignalsPlotter):
     def add_signal_with_analysis(
             self, signal_content: tuple[NDArray, str, NDArray, int, NDArray, NDArray, str]
     ) -> None:
-        """Add signal to signals_set with its tas, qrs_peaks and frequency and wavelet transform."""
+        """Add signal to signals_set with its tas, qrs_peaks, frequency and wavelet transform."""
         self.signals_set.append(signal_content)
-
-    def get_max_num_levels(self) -> int:
-        """Get maximum number of levels for wavelets decomposition."""
-        max_num_levels = 0
-        for signal, tag, qrs_peaks, freq, cA, cDs, wavelet in self.signals_set:
-            cDs_number = len(cDs)
-            if cDs_number > max_num_levels:
-                max_num_levels = cDs_number
-
-        return max_num_levels
 
     def compute_plotting(self) -> None:
         """Plot all signals added to signals plotter."""
 
         squeeze_when_one_signal = False
 
-        num_levels = self.get_max_num_levels()
+        num_levels = self.get_max_num_decomposition_levels()
         num_signals = len(self.signals_set)
 
         fig, axs = plt.subplots(
-            nrows=num_levels + 2,
+            nrows=1 + num_levels,
             ncols=num_signals + 1,
-            figsize=(6 * num_signals, 2 * (num_levels + 3)),
+            figsize=(6 * (num_signals + 1), 2 * (1 + num_levels)),
             squeeze=squeeze_when_one_signal,
             sharex='col',
             sharey='row',
@@ -48,17 +38,8 @@ class DWTDecompositionPlotter(SignalsPlotter):
 
         for i, (signal, tag, qrs_peaks, freq, cA, cDs, wavelet) in enumerate(self.signals_set):
             duration = len(signal) / freq
-            time_signal = np.linspace(0, duration, num=len(signal))
 
-            signal_ax = axs[0, i]
-            signal_ax.plot(time_signal, signal)
-            signal_ax.set_title(f"ECG: {tag} with QRS (len={len(signal)})")
-            signal_ax.set_xlabel("Time [s]")
-            signal_ax.set_ylabel("Amplitude [mV]")
-            signal_ax.set_xlim(0, duration)
-            signal_ax.grid(True)
-            for qrs in qrs_peaks:
-                signal_ax.axvline(x=qrs / freq, color='r', linestyle='--', alpha=0.1)
+            self.plot_ecg_signal(axs[0, i], signal, duration, freq, tag, qrs_peaks)
 
             for coef_number, coef in enumerate([cA] + cDs):
                 level_name = "A" if coef_number == 0 else f"D{len(cDs) - coef_number + 1}"
@@ -70,18 +51,10 @@ class DWTDecompositionPlotter(SignalsPlotter):
                 ax.set_xlim(0, duration)
                 ax.grid(True)
 
-            wavelet_function = pywt.Wavelet(wavelet)
-            phi, psi, x = wavelet_function.wavefun()
-
-            if i + 1 < num_levels + 2:
-                ax_wave = axs[i, -1]
-                ax_wave.plot(x, psi)
-                ax_wave.set_title(f"Wavelet for {tag} ψ(t) – {wavelet}")
-                ax_wave.set_xlabel("t")
-                ax_wave.set_ylabel("ψ(t)")
-                ax_wave.grid(True)
-            else:
-                print(f'Skipped plotting wavelet {wavelet} for {tag}, no space for it')
+                if i < axs.shape[0]:
+                    self.plot_wavelet(axs[i, -1], wavelet, tag)
+                else:
+                    print(f'Skipped plotting wavelet {wavelet} for {tag}, no space for it')
 
         for i in range(num_levels + 1, num_signals - 1, -1):
             axs[i, -1].axis('off')
--- plotters/signals_plotter.py
+++ plotters/signals_plotter.py
@@ -3,7 +3,10 @@
 from abc import ABC, abstractmethod
 
 import matplotlib
+import numpy as np
+import pywt
 from matplotlib import pyplot as plt
+from numpy._typing import NDArray
 
 matplotlib.use("TkAgg")
 
@@ -12,11 +15,51 @@ class SignalsPlotter(ABC):
     """SignalsPlotter class for specific signals plotters that contains main implementations."""
 
     @abstractmethod
-    def compute_plotting(self):
-        """Plot all needed elements."""
+    def add_signal_with_analysis(self):
+        """Add signal to signals_set with its tag, qrs_peaks and frequency and wavelet transform."""
 
-    def plot_ecg_signal(self):
-        """Plot ecg signal."""
+    @abstractmethod
+    def compute_plotting(self, signal_content: tuple):
+        """Plot signal with his specific analysis."""
+
+    def get_max_num_decomposition_levels(self) -> int:
+        """Get the maximum number of levels for wavelets decomposition."""
+        max_num_levels = 0
+        for signal_data in self.signals_set:
+            cDs = signal_data[5]
+            cDs_number = len(cDs)
+            if cDs_number > max_num_levels:
+                max_num_levels = cDs_number
+
+        return max_num_levels + 1
+
+    @staticmethod
+    def plot_ecg_signal(plot_ax, signal: NDArray, duration: float, freq: int, tag: str, qrs_peaks: NDArray):
+        """Plot ecg signal with qrs_peaks."""
+
+        time_signal = np.linspace(0, duration, num=len(signal))
+
+        plot_ax.plot(time_signal, signal)
+        plot_ax.set_title(f"ECG: {tag} with QRS (l={len(signal)})")
+        plot_ax.set_xlabel("Time [s]")
+        plot_ax.set_ylabel("Amplitude [mV]")
+        plot_ax.set_xlim(0, duration)
+        plot_ax.grid(True)
+        for qrs in qrs_peaks:
+            plot_ax.axvline(x=qrs / freq, color='r', linestyle='--', alpha=0.1)
+
+    @staticmethod
+    def plot_wavelet(plot_ax, wavelet: str, tag: str):
+        """Plot wavelet."""
+
+        wavelet_function = pywt.Wavelet(wavelet)
+        phi, psi, x = wavelet_function.wavefun()
+
+        plot_ax.plot(x, psi)
+        plot_ax.set_title(f"Wavelet for {tag} ψ(t) – {wavelet}")
+        plot_ax.set_xlabel("t")
+        plot_ax.set_ylabel("ψ(t)")
+        plot_ax.grid(True)
 
     @staticmethod
     def display_plots():
