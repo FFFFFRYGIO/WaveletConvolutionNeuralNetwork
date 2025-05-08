5b955ebda6e5cc1616a79961bec63695a6806037
FFFFFRYGIO <42962305+FFFFFRYGIO@users.noreply.github.com>
2025-05-08 22:39:27 +0200

    update a plotting mechanism for wavelet_decomposition_parameters_analysis.py

--- wavelet_transform_module/wavelet_decomposition_parameters_analysis.py
+++ wavelet_transform_module/wavelet_decomposition_parameters_analysis.py
@@ -10,7 +10,7 @@ matplotlib.use("TkAgg")
 
 def view_wavelet_filters(wavelet: Wavelet):
     """View default wavelet filters."""
-    print(f'Analysing {wavelet.name=}')
+    print(f'Analysing Wavelet: {wavelet}')
 
     print("Decomposition low-pass filter (h):", wavelet.dec_lo)
     print("Decomposition high-pass filter (g):", wavelet.dec_hi)
@@ -18,60 +18,77 @@ def view_wavelet_filters(wavelet: Wavelet):
     print("Reconstruction high-pass filter (ĝ):", wavelet.rec_hi)
 
 
-def plot_wavelet(plot_ax, ord_number, wavelet_name, wavelet_basic: Wavelet, wavelet_custom: Wavelet):
+def plot_wavelet(scaling_plot, wavelet_plot, wavelet: Wavelet):
     """Plot wavelet scaling and base function."""
-    print(f'Plotting\n  BASIC: {wavelet_basic}\n  CUSTOM: {wavelet_custom}')
+    print(f'Plotting Wavelet: {wavelet}')
 
-    for row, wavelet in enumerate((wavelet_basic, wavelet_custom)):
-        phi, psi, x = wavelet.wavefun()[:3]
+    phi, psi, x = wavelet.wavefun()[:3]
 
-        scaling_plot = plot_ax[row, 0 + 2 * ord_number]
-        scaling_plot.plot(x, phi)
-        scaling_plot.set_title(f"{wavelet_name} Scaling Function φ(x)")
-        scaling_plot.set_xlabel("x")
-        scaling_plot.set_ylabel("φ(x)")
-        scaling_plot.grid(True)
+    scaling_plot.plot(x, phi)
+    scaling_plot.set_title(f"{wavelet.name} Scaling φ(x)")
+    scaling_plot.set_xlabel("x")
+    scaling_plot.set_ylabel("φ(x)")
+    scaling_plot.grid(True)
 
-        wavelet_plot = plot_ax[row, 1 + 2 * ord_number]
-        wavelet_plot.plot(x, psi)
-        wavelet_plot.set_title(f"{wavelet_name} Wavelet Function ψ(x)")
-        wavelet_plot.set_xlabel("x")
-        wavelet_plot.set_ylabel("ψ(x)")
-        wavelet_plot.grid(True)
+    wavelet_plot.plot(x, psi)
+    wavelet_plot.set_title(f"{wavelet.name} Wavelet ψ(x)")
+    wavelet_plot.set_xlabel("x")
+    wavelet_plot.set_ylabel("ψ(x)")
+    wavelet_plot.grid(True)
 
 
 if __name__ == '__main__':
-    wavelets_list = ['db4']
+    wavelets_list = ['db4', 'db8', 'sym4']
 
-    fig, axs = plt.subplots(
-        nrows=2,
-        ncols=len(wavelets_list) * 2,
-        figsize=(4 * 2, 3 * len(wavelets_list) * 2),
-        squeeze=False,
-        sharex=True,
-        sharey=True,
-    )
+    plot_custom_wavelet = True
 
-    for i, wavelet_name in enumerate(wavelets_list):
-        wavelet_basic = pywt.Wavelet(wavelet_name)
-        view_wavelet_filters(wavelet_basic)
-
-        base_lo = np.array(wavelet_basic.dec_lo, dtype=float)
-        new_lo = base_lo.copy()
-        new_lo[0] *= 1.0001
-        new_lo *= (np.sqrt(2) / new_lo.sum())
-
-        dec_hi = qmf(new_lo)
-        dec_lo, rec_lo, rec_hi = new_lo, new_lo[::-1], dec_hi[::-1]
-
-        wavelet_custom = Wavelet(
-            wavelet_name + "_tweaked",
-            filter_bank=(dec_lo, dec_hi, rec_lo, rec_hi)
+    if plot_custom_wavelet:
+        fig, axs = plt.subplots(
+            nrows=2,
+            ncols=len(wavelets_list) * 2,
+            figsize=(10 * 2, 2 * len(wavelets_list)),
+            squeeze=False,
+            sharex=True,
+            sharey=True,
         )
 
-        view_wavelet_filters(wavelet_custom)
+        for i, wavelet_name in enumerate(wavelets_list):
+            wavelet_basic = pywt.Wavelet(wavelet_name)
 
-        plot_wavelet(axs, i, wavelet_name, wavelet_basic, wavelet_custom)
+            view_wavelet_filters(wavelet_basic)
+            plot_wavelet(axs[0, 0 + 2 * i], axs[0, 1 + 2 * i], wavelet_basic)
+
+            base_lo = np.array(wavelet_basic.dec_lo, dtype=float)
+            new_lo = base_lo.copy()
+            new_lo[0] *= 1.0001
+            new_lo *= (np.sqrt(2) / new_lo.sum())
+
+            dec_hi = qmf(new_lo)
+            dec_lo, rec_lo, rec_hi = new_lo, new_lo[::-1], dec_hi[::-1]
+
+            wavelet_custom = Wavelet(
+                wavelet_name + "_tweaked",
+                filter_bank=(dec_lo, dec_hi, rec_lo, rec_hi)
+            )
+
+            view_wavelet_filters(wavelet_custom)
+            plot_wavelet(axs[1, 0 + 2 * i], axs[1, 1 + 2 * i], wavelet_custom)
+
+    else:
+        fig, axs = plt.subplots(
+            nrows=len(wavelets_list),
+            ncols=2,
+            figsize=(2 * len(wavelets_list), 4 * 2),
+            squeeze=False,
+            sharex=True,
+            sharey=True,
+        )
+
+        for i, wavelet_name in enumerate(wavelets_list):
+            wavelet_basic = pywt.Wavelet(wavelet_name)
+            view_wavelet_filters(wavelet_basic)
+
+            plot_wavelet(axs[i, 0], axs[i, 1], wavelet_basic)
 
     plt.tight_layout()
     plt.show()
