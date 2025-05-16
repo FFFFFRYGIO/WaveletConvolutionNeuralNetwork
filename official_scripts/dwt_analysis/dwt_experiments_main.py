415ad6cb951864f45e9b97dc1eb4fc2e16c404f5
FFFFFRYGIO <42962305+FFFFFRYGIO@users.noreply.github.com>
2025-05-16 03:10:35 +0200

    add somme more details for dwt_experiments_main.py

--- official_scripts/dwt_analysis/dwt_experiments_main.py
+++ official_scripts/dwt_analysis/dwt_experiments_main.py
@@ -9,19 +9,23 @@ from official_scripts.dwt_analysis.experiments_plotter import DWTExperimentsPlot
 def main():
     """Run DWT and IDWT experiments."""
     seconds = 5
-    signal_amounts = {'ARR': 0, 'CHF': 0, 'NSR': 1}
+    signal_amounts = {'ARR': 1, 'CHF': 1, 'NSR': 1}
     normalize_denoise_combinations = [[True, False]]  # [[False, False], [True, False], [True, True], [False, True]]
-    wavelets = ['db4']
+    wavelets = ['db4', 'sym4']
     decomposition_levels = 3
     reconstruction_combinations_set: list[list[str]] = []
 
     signals_data, frequency = get_signals_list(seconds, signal_amounts)
 
     signals_contents_objects_list: list[ECGSignalContent] = []
+
     for wavelet in wavelets:
         for signal, tag in signals_data:
             for normalize, denoise in normalize_denoise_combinations:
                 signal_object = ECGSignalContent(signal, tag, frequency, wavelet, normalize, denoise)
+                signal_object.run_dwt(decomposition_levels)
+                signal_object.set_reconstruction_combinations(combinations=reconstruction_combinations_set)
+                signal_object.run_idwt()
                 signals_contents_objects_list.append(signal_object)
 
     plotter = DWTExperimentsPlotter(signals_contents_objects_list, frequency)
