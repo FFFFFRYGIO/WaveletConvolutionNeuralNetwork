4fb5b80cf4fdfb70df5baae36626265616e5333c
FFFFFRYGIO <42962305+FFFFFRYGIO@users.noreply.github.com>
2025-05-18 19:34:31 +0200

    add template applications for different dwt experiments

---
+++ official_scripts/dwt_analysis/researches/dwt_exp_raw_denoised.py
@@ -0,0 +1,23 @@
+"""Run various experiments using ECGSignal class and dwt_features script."""
+from dwt_features import get_signals_list, run_signals_analysis
+
+
+def main():
+    """Run DWT and IDWT experiments."""
+    seconds: int | None = 10
+    signal_amounts: dict[str, int] = {'ARR': 1, 'CHF': 1, 'NSR': 1}
+    signals_data, frequency = get_signals_list(seconds, signal_amounts)
+
+    wavelets_list: list[str] = ['db4']
+    decomposition_levels: list[int] = [4]
+    denoise_combinations: set[bool] = {False, True}
+    reconstruction_combinations_set: list[str | list[str]] = ['first_two', 'second_two']
+
+    run_signals_analysis(
+        signals_data, frequency, wavelets_list, decomposition_levels,
+        denoise_combinations, reconstruction_combinations_set,
+    )
+
+
+if __name__ == '__main__':
+    main()
