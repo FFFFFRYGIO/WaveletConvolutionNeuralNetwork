70f46de35703b18d67a48723055d329b97033953
FFFFFRYGIO <42962305+FFFFFRYGIO@users.noreply.github.com>
2025-05-26 02:47:55 +0200

    get level name for logger from environment variables

--- wavelet_neural_network/official_scripts/logger.py
+++ wavelet_neural_network/official_scripts/logger.py
@@ -1,9 +1,10 @@
 """Logger object for logging data operation, network creation and learning processes."""
 import logging
+import os
 import sys
 
 logging.basicConfig(
-    level=logging.INFO,
+    level=os.getenv('LOG_LEVEL', 'INFO'),
     stream=sys.stdout,
     format='[%(asctime)s] %(levelname)s %(funcName)s: %(message)s',
     datefmt='%y-%m-%d %H:%M:%S'
