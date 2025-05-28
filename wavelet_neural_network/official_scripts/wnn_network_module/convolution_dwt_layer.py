6ad49294bedbd8f2fd9d02b4fd20a9b8b9a85b4e
FFFFFRYGIO <42962305+FFFFFRYGIO@users.noreply.github.com>
2025-05-29 00:57:12 +0200

    revert convolution_dwt_layer.py back to template form

--- wavelet_neural_network/official_scripts/wnn_network_module/convolution_dwt_layer.py
+++ wavelet_neural_network/official_scripts/wnn_network_module/convolution_dwt_layer.py
@@ -4,10 +4,9 @@ from copy import deepcopy
 import numpy as np
 import pywt
 import torch
-from logger import logger
-from pytorch_wavelets import DWT1D, IDWT1D
-from torch import Tensor
 from torch import nn
+
+from logger import logger
 
 
 class WaveletDWTLayer(nn.Module):
@@ -23,7 +22,7 @@ class WaveletDWTLayer(nn.Module):
         self.filler_value = filler_value  # Out from <-10,10> range, to tell that this is only a filler
 
     @staticmethod
-    def torch_qmf(wavelet_filter: Tensor) -> Tensor:
+    def torch_qmf(wavelet_filter: torch.Tensor) -> torch.Tensor:
         """Returns the Quadrature Mirror Filter (QMF) of the input tensor.
         Refactored from PyWavelets library:
         https://pywavelets.readthedocs.io/en/latest/ref/other-functions.html#pywt.qmf"""
@@ -34,8 +33,8 @@ class WaveletDWTLayer(nn.Module):
         return qm_filter
 
     def torch_orthogonal_filter_bank(
-            self, scaling_filter: Tensor
-    ) -> tuple[Tensor, Tensor, Tensor, Tensor]:
+            self, scaling_filter: torch.Tensor
+    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
         """Returns the orthogonal filter bank for the given scaling filter tensor.
         Refactored from PyWavelets library:
         https://pywavelets.readthedocs.io/en/latest/ref/other-functions.html#pywt.orthogonal_filter_bank"""
@@ -64,44 +63,30 @@ class WaveletDWTLayer(nn.Module):
         new_wavelet.biorthogonal = True
         self.wavelet = new_wavelet
 
-    def run_dwt(self, signal: Tensor) -> tuple[Tensor, Tensor]:
-        """Run DWT using pytorch_wavelets."""
-        if signal.dim() == 2:  # (N, L)
-            signal = signal.unsqueeze(1)  # → (N, 1, L)
-        elif signal.dim() != 3:
-            raise ValueError(f"Expected 2D or 3D tensor, got {signal.dim()}D")
-
-        dec_lo, dec_hi, rec_lo, rec_hi = self.torch_orthogonal_filter_bank(self.weights)
-        dwt1d = DWT1D(wave=(dec_lo.tolist(), dec_hi.tolist()), mode='symmetric')
-        cA, cD_list = dwt1d(signal)
-        cD = cD_list[0]
-
+    def run_dwt(self, signal: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+        """Run DWT on input signal."""
+        cA, cD = pywt.dwt(signal, self.wavelet)
         return cA, cD
 
     def run_idwt(
-            self, cA: Tensor, cD: Tensor, cDs: Tensor, number_coeffs_for_rec: int = 2
-    ) -> Tensor:
-        """Run IDWT using pytorch_wavelets."""
-        dec_lo, dec_hi, rec_lo, rec_hi = self.torch_orthogonal_filter_bank(self.weights)
-        idwt1d = IDWT1D(wave=(rec_lo.tolist(), rec_hi.tolist()), mode='symmetric')
+            self, cA: np.ndarray, cD: np.ndarray, cDs: list[np.ndarray], number_coeffs_for_rec: int = 2
+    ) -> np.ndarray:
+        """Run inverse DWT based on a coefficient list."""
+        waverec_coeffs = [np.zeros_like(cA)]
 
-        cA_t = torch.zeros_like(cA)
-        cDs_t: list[Tensor] = []
-
-        for cD_number, cD_with_pad in enumerate(cDs.unbind()):
-            non_filler_mask = torch.ne(cD_with_pad, self.filler_value)
-            cD = cD_with_pad.masked_select(non_filler_mask).view(1, 1, -1)
-            if cD_number in range(number_coeffs_for_rec):
-                cDs_t.append(cD)
+        for coeff_number, coeff_with_pad in enumerate(cDs[::-1]):
+            coeff = coeff_with_pad[np.abs(coeff_with_pad - self.filler_value) > 1e-4].reshape(1, -1)
+            if coeff_number in range(number_coeffs_for_rec):
+                waverec_coeffs.append(coeff)
             else:
-                cDs_t.append(torch.zeros_like(cD))
+                waverec_coeffs.append(np.zeros_like(coeff))
 
-        rec = idwt1d((cA_t, cDs_t))
-        return rec
+        R = pywt.waverec(waverec_coeffs, self.wavelet)
+        return R
 
     def run_wavelet_computation(
-            self, signal: Tensor, cDs: Tensor, reconstructions: Tensor
-    ) -> tuple[Tensor, Tensor, Tensor]:
+            self, signal: np.ndarray, cDs: list[np.ndarray], reconstructions: list[np.ndarray]
+    ) -> tuple[np.ndarray, list[np.ndarray], list[np.ndarray]]:
         """Run DWT and IDWT and return result for a batch element."""
 
         # Update wavelet with new weights as filter
@@ -112,43 +97,26 @@ class WaveletDWTLayer(nn.Module):
 
         # Add cD to cDs
         if self.layer_number == 1:
-            cDs = cD
+            cDs = np.array([cD])
         else:
-            pad = cD.new_full((*cD.shape[:-1], cDs.shape[-1] - cD.shape[-1]), self.filler_value)
-            cD_padded = torch.cat([cD, pad], dim=-1)
-            cDs = torch.cat([cDs, cD_padded])
+            cD_padded = np.pad(cD, ((0, 0), (0, cDs[0].size - cD.size)), constant_values=self.filler_value)
+            cDs.append(cD_padded)
 
         # Run IDWT and add new reconstruction
         if not self.layer_number == 1:
             R = self.run_idwt(cA, cD, cDs)
             if self.layer_number == 2:
-                reconstructions = R
+                reconstructions = np.array([R])
             else:
-                reconstructions = torch.cat([reconstructions, R])
+                reconstructions.append(R)
 
         return cA, cDs, reconstructions
 
-    def forward(self, x1: Tensor, x2: Tensor, x3: Tensor) -> tuple[Tensor, Tensor, Tensor]:
+    def forward(
+            self, x1: torch.Tensor, x2: torch.Tensor, x3: torch.Tensor,
+    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
         """Process each batch element by running DWT and IDWT."""
         logger.debug('{module_name} forward {layer_num}'.format(
             module_name=self.__class__.__name__, layer_num=self.layer_number)
         )
-        signal = x1.unbind()
-        cDs = x2.unbind()
-        reconstructions = x3.unbind()
-
-        out_cA_list = []
-        out_cDs_list = []
-        out_recs_list = []
-
-        for signal, cDs, recs in zip(signal, cDs, reconstructions):
-            cA_i, cDs_i, rec_i = self.run_wavelet_computation(signal, cDs, recs)
-            out_cA_list.append(cA_i)
-            out_cDs_list.append(cDs_i)
-            out_recs_list.append(rec_i)
-
-        cA_batch = torch.stack(out_cA_list)
-        cDs_batch = torch.stack(out_cDs_list)
-        recs_batch = torch.stack(out_recs_list)
-
-        return cA_batch, cDs_batch, recs_batch
+        pass
